2ef07a95fb472eb972d8125304dc53f0c52e6a2e
Lautaro <ohorodeski@gmail.com>
2025-04-24 21:30:41 -0300

    Leandro aprobala pto

--- Programacion_III/Unidad_II/src/ejercicios_arreglos/Ejercicio6.java
+++ Programacion_III/Unidad_II/src/ejercicios_arreglos/Ejercicio6.java
@@ -1,7 +1,21 @@
 package ejercicios_arreglos;
-
+import java.util.Locale;
+import  java.util.Scanner;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
 public class Ejercicio6 {
     public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    LocalDate fechaActual = LocalDate.now();
+    DateTimeFormatter formatoddMMyyy = java.time.format.DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
+        for (int i = 0; i < 7; i++) {
+        String fechaFormateada = fechaActual.plusDays(i).format(formatoddMMyyy);
+            LocalDate fecha = fechaActual.plusDays(i);
+            DayOfWeek diaSemana = fecha.getDayOfWeek();
+            String diaEnEspanol = diaSemana.getDisplayName(java.time.format.TextStyle.FULL, Locale.forLanguageTag("es-ES"));
+            System.out.println(diaEnEspanol +" "+ fechaFormateada);
+        }
     }
 }
